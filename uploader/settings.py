ebf65beee005efa6d5c9ef7beb2cbdb9f045bb5d
3irna <arya.anrays.20@gmail.com>
2024-11-12 01:26:44 +0330

    fix settings file

--- uploader/settings.py
+++ uploader/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7k4_0y9^%3qr+)hd1uf^)(q9v&fhgm1)lk-xvxa=t62w18$0^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
